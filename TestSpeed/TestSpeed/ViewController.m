98589f5f14c96d45b9a92f4400a07fbcb476eafc
liuxu <liuxu@liuxudeMacBook-Pro.local>
2016-01-08 17:56:04 +0800

    update

--- TestSpeed/TestSpeed/ViewController.m
+++ TestSpeed/TestSpeed/ViewController.m
@@ -7,8 +7,16 @@
 //
 
 #import "ViewController.h"
+#import <CoreMotion/CoreMotion.h> 
+
+void messageBox(NSString* str) {
+    UIAlertView* view = [[UIAlertView alloc]initWithTitle:@"debug" message:str delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
+    [view show];
+}
 
 @interface ViewController ()
+
+@property (nonatomic,strong) CMMotionManager* motionManager;
 
 @end
 
@@ -16,7 +24,25 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    __block NSString* str = nil;
+    self.motionManager=[[CMMotionManager alloc]init];
+    self.motionManager.gyroUpdateInterval=0.1;
+    [self.motionManager startGyroUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMGyroData *gyroData, NSError *error) {
+        str = [NSString stringWithFormat:@"旋转角度:X:%.3f,Y:%.3f,X:%.3f",gyroData.rotationRate.x,gyroData.rotationRate.y,gyroData.rotationRate.z];
+        messageBox(str);
+    }];
+    
+    self.motionManager.accelerometerUpdateInterval=0.1;
+    [self.motionManager startAccelerometerUpdatesToQueue:[NSOperationQueue currentQueue] withHandler:^(CMAccelerometerData *accelerometerData, NSError *error) {
+        str = [NSString stringWithFormat:@"加速计:X:%.3f,Y:%.3f,Z:%.3f",accelerometerData.acceleration.x,accelerometerData.acceleration.y,accelerometerData.acceleration.z];
+        messageBox(str);
+    }];
+}
+
+- (void)stop {
+    [self.motionManager stopAccelerometerUpdates];
+    [self.motionManager stopGyroUpdates];
 }
 
 - (void)didReceiveMemoryWarning {

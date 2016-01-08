b5eb70e2b11b04adaf72395253a727e05821f2c6
liuxu <liuxu@liuxudeMacBook-Pro.local>
2016-01-08 17:46:40 +0800

    添加速度测试工程—测试研究

---
+++ TestSpeed/TestSpeed/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestSpeed
+//
+//  Created by liuxu on 16/1/8.
+//  Copyright (c) 2016年 liuxu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}

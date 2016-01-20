4621c5ff4bae8e1c70825e47cec5c6e57300e8af
zhenghong <332313547@qq.com>
2016-01-20 11:14:45 +0800

    Revert "zhenghong：update"
    This reverts commit ab3cf91c869c92a5bc664d47dcf7ce4e473640e2.

--- DataMining/DataMining/CollectDataManager/CollectDataManager.m
+++ DataMining/DataMining/CollectDataManager/CollectDataManager.m
@@ -14,14 +14,21 @@
 #import "TouchWorker.h"
 #import "LocationWorker.h"
 
+<<<<<<< Updated upstream
 #define kSpeedFileName      @"加速计.txt"
 #define kAngleFileName      @"旋转角度.txt"
 #define kStepFileName       @"实时步数.txt"
 #define kDistanceFileName   @"实时距离.txt"
 #define mAngleFileName      @"角度.txt"
 #define kTouchFileName      @"触摸.txt"
+=======
+#define kSpeedFileName  @"加速计.txt"
+#define kAngleFileName  @"旋转角度.txt"
+#define kStepFileName   @"实时步数.txt"
+#define mAngleFileName  @"角度.txt"
+#define kTouchFileName  @"触摸.txt"
 #define kLocationFileName @"location.txt"
-
+>>>>>>> Stashed changes
 
 void messageBox(NSString* str) {
     UIAlertView* view = [[UIAlertView alloc]initWithTitle:@"" message:str delegate:nil cancelButtonTitle:@"OK" otherButtonTitles:nil, nil];
